b7f9a563b199dcdca2dab3c9e1bf0c8b58cc9088
Cherkaoui El Wafi <elwafi.cherkaoui@gmail.com>
2024-04-11 00:10:48 +0000

    Connect UI with the game logic
    After testing the game logic via the console,
    I've made improvements to the user experience by:
    
        - Switching from prompt inputs to event listeners
        for smoother interaction.
        - Using an Immediately Invoked Function Expression (IIFE)
        to organize all code's data and objects.
        - Removing the 'wins' attribute from the Player object
        to simplify data management.
        - Separating round wins from overall game wins
        to avoid bugs.
    
    These upgrades not only improve the game's functionality
    but also set the stage for future development and maintenance.

--- main.js
+++ main.js
@@ -1,133 +1,167 @@
-const gameboard = (function(){
-    let board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
-    let set_board = function(row, col, sign){
-        board[row][col] = sign
-    }
-    let get_board = ()=>{
-        console.log(board)
-    }
-    let reset_board = ()=>{
-        board = [
+let  tic_tac_toe = (function(){
+    const gameboard = (function(){
+        let board = [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]
         ]
-    }
-    let check_wins = ()=>{
-        let ended = false
-        let row = 0
-        let diag_1_sum = 0
-        let diag_2_sum = 0
-        while(row < 3 && !ended){
-            let row_sum = 0
-            let col_sum = 0
-            for (let i = 0; i<3; i++){
-                row_sum += board[row][i]
-                col_sum += board[i][row]
-
-                if (i == row) {diag_1_sum += board[i][row]}
-                if (i + row == 2) {diag_2_sum += board[row][i]}
-                console.log('row',row_sum,'col', col_sum);
-                // console.log('d1',diag_1_sum,'d2', diag_2_sum);
-            }
-            let sums = [row_sum, col_sum, diag_1_sum, diag_2_sum]
-            sums.forEach(sum => {
-                if(sum == 3 || sum == -3){
-                    alert('ended')
-                    ended = true
+        let set_board = function(row, col, sign){
+            board[row][col] = sign
+        }
+        let get_board = ()=>{
+            console.log(board)
+        }
+        let reset_board = ()=>{
+            board = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ]
+        }
+        let check_wins = ()=>{
+            let ended = false
+            let row = 0
+            let diag_1_sum = 0
+            let diag_2_sum = 0
+            while(row < 3 && !ended){
+                let row_sum = 0
+                let col_sum = 0
+                for (let i = 0; i<3; i++){
+                    row_sum += board[row][i]
+                    col_sum += board[i][row]
+    
+                    if (i == row) {diag_1_sum += board[i][row]}
+                    if (i + row == 2) {diag_2_sum += board[row][i]}
+                    // console.log('row',row_sum,'col', col_sum);
+                    // console.log('d1',diag_1_sum,'d2', diag_2_sum);
                 }
+                let sums = [row_sum, col_sum, diag_1_sum, diag_2_sum]
+                sums.forEach(sum => {
+                    if(sum == 3 || sum == -3){
+                        alert('ended')
+                        ended = true
+                    }
+                });
+                row += 1
+            }
+            if (ended) return true
+            else return false
+        }
+    
+        return {set_board, get_board,reset_board, check_wins}
+    })()
+    
+    
+    function Game(player1, player2){
+        let round_count = 0
+        let player_I = {player: player1, round_wins: 0, game_wins: 0}
+        let player_II = {player: player2, round_wins: 0, game_wins: 0}
+        let game_winner = ''
+        let ties_count = 0
+        let get_winner = ()=>{
+            return game_winner
+        }
+        let get_player_I = ()=> player_I
+        let get_player_II = ()=> player_II
+        let start_game = ()=>{
+            let gameboard_div = document.querySelectorAll('.gameboard .cell')
+            let cell_number = 1
+            let row_number = 1
+            gameboard_div.forEach(cell => {
+                cell.className += ' c'+cell_number
+                cell_number += 1
+                if(cell_number > 3) {
+                    cell_number = 1
+                    cell.parentNode.className += ' r'+row_number
+                    row_number += 1
+                }
+                cell.addEventListener('click', (event)=>{
+                    let row = event.target.parentNode.className
+                    let col = event.target.className
+                    row = row.charAt(row.length-1) - 1
+                    col = col.charAt(col.length-1) - 1
+                    console.log(round_count);
+                    start_round(row, col)
+                    cell.removeEventListener('click', cell)
+                    if (round_count == 9){
+                        its_tie()
+                        end_turn()
+                        player_I.round_wins = 0
+                        player_II.round_wins = 0
+                        alert('its tie')
+                        return 1
+                    }
+                    
+                })
+    
+    
             });
-            row += 1
+            // while(1){
+            //     start_round()
+            // }
+    
         }
-        if (ended) return true
-        else return false
-    }
-    return {set_board, get_board,reset_board, check_wins}
-})()
-
-
-function Game(player1, player2){
-    let player_I = {player: player1, wins: 0}
-    let player_II = {player: player2, wins: 0}
-    let game_winner = ''
-    let ties_count = 0
-    let get_winner = ()=>{
-        return game_winner
-    }
-    let start_game = ()=>{
-        while(1){
-            start_round()
-            if (player_I.wins == 3){
-                game_winner = player1
-                break
-            }
-            if (player_II.wins == 3){
-                game_winner = player2
-                break
-            }
-        }
-
-    }
-    let start_round = ()=>{
-        let players = [player_I, player_II]
-        for (let i = 0; i<9; i++){
-            on_round = players[i%2]
-            let row = Number(prompt('enter index of row'))-1
-            let col = Number(prompt('enter index of col'))-1
+        let start_round = (row, col)=>{
+            let players = [player_I, player_II]
+            let on_round = players[round_count%2]
             gameboard.set_board(row,col, on_round.player.get_sign())
             gameboard.get_board()
             let won = gameboard.check_wins()
             if (won) {
                 alert(`${on_round.player.name} has won`)
-                on_round.wins += 1
-                players.splice(i%2,1)
-                players[0].wins = 0
+                on_round.round_wins += 1
+                players.splice(round_count%2,1)
+                players[0].round_wins = 0
                 end_turn()
+                if (player_I.round_wins == 3){
+                    winner(player_I)
+                }
+                if (player_II.round_wins == 3){
+                    winner(player_II)
+                }
                 return 1
             }
+            round_count += 1
+            
+    
         }
-        its_tie()
-        player_I.wins = 0
-        player_II.wins = 0
-        alert('its tie')
-
-    }
-    let end_turn = ()=>{
-        gameboard.reset_board()
-        console.log(player_I)
-        console.log(player_II)
-    }
-    let its_tie = ()=>{
-        ties_count += 1
-    }
-    let get_ties = ()=>{
-        return ties_count
+        let end_turn = ()=>{
+            round_count = 0
+            gameboard.reset_board()
+            // console.log(player_I)
+            // console.log(player_II)
+        }
+        let its_tie = ()=>{
+            ties_count += 1
+        }
+        let get_ties = ()=>{
+            return ties_count
+        }
+        let winner = (player)=>{
+            player.round_wins = 0
+            player.game_wins += 1
+            console.log(player);
+        }
+        
+        return {start_game, get_winner, get_ties, get_player_I, get_player_II}
     }
     
-    return {start_game, get_winner, get_ties}
-}
-
-function Player(name, sign){
-    let player_sign = sign
-    if (player_sign  == 'x') {player_sign = 1}
-    else if (player_sign == 'o') {player_sign = -1}
-
-    let get_sign = ()=>{
-        return player_sign
+    function Player(name, sign){
+        let player_sign = sign
+        if (player_sign  == 'x') {player_sign = 1}
+        else if (player_sign == 'o') {player_sign = -1}
+    
+        let get_sign = ()=>{
+            return player_sign
+        }
+        
+    
+        return {name, get_sign}
     }
-
-
-    return {name, get_sign}
-}
-
-
-let p1 = Player('elwafi', 'x')
-let p2 = Player('ahmed', 'o')
-let p = Game(p1, p2)
-// console.log(gameboard.get_board())
-p.start_game()
-
+    
+    let p1 = Player('elwafi', 'x')
+    let p2 = Player('ahmed', 'o')
+    let p = Game(p1, p2)
+    // console.log(gameboard.get_board())
+    p.start_game()
+})()
